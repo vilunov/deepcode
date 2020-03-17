d7295318780d0e1d0780661533a11e6190c8df20
Nikita Vilunov <n.vilyunov@tinkoff.ru>
2020-03-17 09:55:54 +0300

    predicting from pickle files

--- model/deepcode/predict/__main__.py
+++ model/deepcode/predict/__main__.py
@@ -1,10 +1,8 @@
 import argparse
 import csv
-import gzip
-import json
+import pickle
 
 import torch as t
-import wandb
 import pandas as pd
 from annoy import AnnoyIndex
 
@@ -43,10 +41,8 @@ class LangIndex:
         self.urls.append(url)
 
 
-def main(enable_wandb: bool = False):
+def main():
     args = arguments()
-    if enable_wandb:
-        wandb.init(name="test")
     with open(args.config, "r") as f:
         config = parse_config(f.read())
     with open(args.queries, "r") as f:
@@ -56,27 +52,23 @@ def main(enable_wandb: bool = False):
     queries = scaffold.tokenizer_doc.encode_batch(queries_text)
     encoded_doc = [scaffold.model.encoder_doc(t.tensor([q.ids]), t.ones(1, len(q), dtype=t.bool)) for q in queries]
 
-    languages = config.model.code_encoder.keys()
-    indices = {lang: LangIndex(config.model.encoded_dims, "angular") for lang in languages}
+    predictions = []
     for data_file in args.data:
-        with gzip.open(data_file, "r") as f:
-            data = f.readlines()
-        data = [json.loads(d) for d in data]
+        index = LangIndex(config.model.encoded_dims, "angular")
+        with open(data_file, "rb") as f:
+            data = pickle.load(f)
+        language: str = ""
         for i, snippet in enumerate(data):
             language = snippet["language"]
             tokenizer = scaffold.tokenizers_code[language]
             encoder = scaffold.model.encoders_code[language]
-            tokens = snippet["code_tokens"]
+            tokens = snippet["function_tokens"]
             tokens = tokenizer.encode_batch(tokens)
             tokens = t.cat([t.tensor(i.ids, dtype=t.int64) for i in tokens])
             repr = encoder(tokens.unsqueeze(0), t.ones(1, tokens.shape[0], dtype=t.bool))
-            indices[language].append(repr[0], snippet["func_name"], snippet["url"])
+            index.append(repr[0], snippet["identifier"], snippet["url"])
+        index.index.build(10)
 
-    for i in indices.values():
-        i.index.build(10)
-
-    predictions = []
-    for language, index in indices.items():
         for query_id, query_repr in enumerate(encoded_doc):
             query = queries_text[query_id]
             ranked_ids = index.index.get_nns_by_vector(query_repr[0], 100)
@@ -87,8 +79,6 @@ def main(enable_wandb: bool = False):
 
     predictions = pd.DataFrame(predictions, columns=["query", "language", "identifier", "url"])
     predictions.to_csv(args.output, index=False)
-    if enable_wandb:
-        wandb.save(args.output)
 
 
 if __name__ == '__main__':
---
+++ model/deepcode/predict/upload/__main__.py
@@ -0,0 +1,10 @@
+import wandb
+
+
+def main():
+    wandb.init(name="test")
+    wandb.save("model_predictions.csv")
+
+
+if __name__ == '__main__':
+    main()
