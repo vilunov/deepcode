be6e80f7743e3a48f4d4846ee916d65ba5454cd0
Nikita Vilunov <nikita@vilunov.me>
2020-04-09 22:03:04 +0300

    fix predict.sh

--- model/predict.sh
+++ model/predict.sh
@@ -5,7 +5,7 @@ c="../cache/models/2020.04.06_18.24.03-go-java-256/config.toml"
 o="../cache/models/2020.04.06_18.24.03-go-java-256/2.csv"
 python -m deepcode.predict \
   -q ../resources/queries.csv \
-  -l java -l go \
+  -l java go \
   -o "$o" \
   -m "$m" \
   -c "$c"
@@ -15,7 +15,7 @@ c="../cache/models/2020.04.07_06.43.21-go-javascript-256/config.toml"
 o="../cache/models/2020.04.07_06.43.21-go-javascript-256/2.csv"
 python -m deepcode.predict \
   -q ../resources/queries.csv \
-  -l javascript -l go \
+  -l javascript go \
   -o "$o" \
   -m "$m" \
   -c "$c"
@@ -25,7 +25,7 @@ c="../cache/models/2020.04.07_10.28.37-go-php-256/config.toml"
 o="../cache/models/2020.04.07_10.28.37-go-php-256/2.csv"
 python -m deepcode.predict \
   -q ../resources/queries.csv \
-  -l php -l go \
+  -l php go \
   -o "$o" \
   -m "$m" \
   -c "$c"
@@ -35,7 +35,7 @@ c="../cache/models/2020.04.07_17.09.37-go-python-256/config.toml"
 o="../cache/models/2020.04.07_17.09.37-go-python-256/2.csv"
 python -m deepcode.predict \
   -q ../resources/queries.csv \
-  -l python -l go \
+  -l python go \
   -o "$o" \
   -m "$m" \
   -c "$c"
